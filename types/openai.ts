ed7efa364b5abdf5b743594035bf3c9b5c5145e1
mreverson <mreverson91@gmail.com>
2023-07-13 10:18:28 -0700

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -8,6 +8,7 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_4 = 'gpt-4',
+  GPT_3_5_TURBO = 'gpt-3.5-turbo-16k'
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -17,13 +18,19 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
     name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 6000,
+    maxLength: 90000,
+    tokenLimit: 35000,
+  },
+  [OpenAIModelID.GPT_3_5_TURBO]: {
+    id: OpenAIModelID.GPT_3_5_TURBO,
+    name: 'GPT-3.5-turbo-16k',
+    maxLength: 180000,
+    tokenLimit: 3500,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 6000,
+    maxLength: 10000,
+    tokenLimit: 200,
   },
 };
